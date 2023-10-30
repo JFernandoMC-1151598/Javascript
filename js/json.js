19d6d9b40c818dbd5e3d81d62fb5b37470154d28
JFERNANDOMC-1151598 <josefernandomc@ufps.edu.co>
2023-10-30 16:09:05 -0500

    Agregando datos de la api

--- js/json.js
+++ js/json.js
@@ -4,7 +4,7 @@ fetch('https://siaweb-nodejs.carlos-reneren7.repl.co/productos')
   // Crear una tabla HTML
   const table = document.createElement('table');
 // Agregar encabezados a la tabla
-const headers = ['Nombre','Precio','Descripcion'];
+const headers = ['Nombre','Precio','Descripcion','Visualizar'];
 const headerRow = document.createElement('tr');
 headers.forEach(headerText => {
   const header = document.createElement('th');
@@ -15,18 +15,24 @@ table.appendChild(headerRow);
  // Agregar filas de datos a la tabla
  data.forEach(producto => {
     const row = document.createElement('tr');
-    const productonombreCell = document.createElement('td');
-    productonombreCell.textContent = producto.nombre;
-    const precioCell = document.createElement('td');
-    precioCell.textContent = producto.precio;
-    const descripcionCell = document.createElement('td');
-    descripcionCell.textContent = producto.descripcion;
+    const productonombre = document.createElement('td');
+    productonombre.textContent = producto.nombre;
+    const precio = document.createElement('td');
+    precio.textContent = producto.precio;
+    const descripcion = document.createElement('td');
+    descripcion.textContent = producto.descripcion;
 
-    row.appendChild(productonombreCell);
-    row.appendChild(precioCell);
-    row.appendChild(descripcionCell);
+    const verbutton = document.createElement('button');
+    verbutton.textContent= "Ver";
+
+
+    row.appendChild(productonombre);
+    row.appendChild(precio);
+    row.appendChild(descripcion);
+    row.appendChild(verbutton);
 
     table.appendChild(row);
+    
   });
 
   // Agregar la tabla al documento
