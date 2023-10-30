2a96d89abd7cc712899729c686e43c53ce339c61
JFERNANDOMC-1151598 <josefernandomc@ufps.edu.co>
2023-10-30 18:12:17 -0500

    Mejora del codigo simplificado y funcionalidad en botones sin js

--- js/json.js
+++ js/json.js
@@ -1,46 +1,18 @@
-fetch('https://siaweb-nodejs.carlos-reneren7.repl.co/productos')
-.then(response => response.json())
-.then(data => {
-  // Crear una tabla HTML
-  const table = document.createElement('table');
-// Agregar encabezados a la tabla
-const headers = ['Nombre','Precio','Descripcion','Visualizar'];
-const headerRow = document.createElement('tr');
-headers.forEach(headerText => {
-  const header = document.createElement('th');
-  header.textContent = headerText;
-  headerRow.appendChild(header);
-});
-table.appendChild(headerRow);
- // Agregar filas de datos a la tabla
- data.forEach(producto => {
-    const row = document.createElement('tr');
-    const productonombre = document.createElement('td');
-    productonombre.textContent = producto.nombre;
-    const precio = document.createElement('td');
-    precio.textContent = producto.precio;
-    const descripcion = document.createElement('td');
-    descripcion.textContent = producto.descripcion;
+let url = 'https://siaweb-nodejs.carlos-reneren7.repl.co/productos'
+    fetch(url)
+      .then(response => response.json())
+      .then(data => mostrarData(data))
+      .catch(error => console.log(error))
 
-    const verbutton = document.createElement('button');
-    verbutton.textContent= "Ver";
-
-
-    row.appendChild(productonombre);
-    row.appendChild(precio);
-    row.appendChild(descripcion);
-    row.appendChild(verbutton);
-
-    table.appendChild(row);
-    
-  });
-
-  // Agregar la tabla al documento
-  document.body.appendChild(table);
-
-})
-
-
+    const mostrarData = (data) => {
+      console.log(data)
+      let body = ''
+      for (let i = 0; i<data.length; i++){
+        body += `<tr><td>${data[i].nombre}</td><td>${data[i].precio}</td><td>${data[i].descripcion}
+        </td><td><button onclick="location.href = 'ver_producto.html'">Ver</button></td></tr>`
+      }
+      document.getElementById('data').innerHTML = body
+    }
 /*
 get /productos: obtengo la relacion de productos
 get /productos/nombre_producto: obtengo producto por nombre
